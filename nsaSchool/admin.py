8ddb2f5a54a36368ca1a6befc57b216c1b9275ab
tjsepp <tjsepp@gmail.com>
2015-11-03 16:25:57 -0700

    Adding NSA School. This is the school level info for the VMS system

---
+++ nsaSchool/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from models import SchoolYear
+from simple_history.admin import SimpleHistoryAdmin
+from django import forms
+from django.forms import TextInput, Textarea
+from django.db import models
+
+
+class SchoolYearAdmin(admin.ModelAdmin):
+    model = SchoolYear
+    list_display = ('schoolYear','currentYear')
+
+admin.site.register(SchoolYear,SchoolYearAdmin)
